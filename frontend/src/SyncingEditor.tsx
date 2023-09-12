b4a8da1248d7c1306acbebfe1d653c58fc7cd874
Pager07 <sunthapa1@hotmail.com>
2023-09-12 09:07:57 +0100

    simple button to style stuff added. Can be reference for stying stuff later on

--- frontend/src/SyncingEditor.tsx
+++ frontend/src/SyncingEditor.tsx
@@ -1,5 +1,5 @@
 // Import React dependencies.
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { BaseOperation, Descendant, Editor, Operation, createEditor } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
 import io from "socket.io-client";
@@ -10,6 +10,13 @@ const socket = io("http://localhost:4000");
 
 type PropsType = { groupId: string }
 
+const Leaf = ({ attributes, children, leaf }) => {
+    if (leaf.bold) {
+        children = <strong>{children}</strong>
+    }
+    return <span {...attributes}>{children}</span>
+}
+
 
 const SyncingEditor = ({ groupId }: PropsType) => {
     const [editor] = useState(() => withReact(createEditor()));
@@ -17,6 +24,7 @@ const SyncingEditor = ({ groupId }: PropsType) => {
     const [value, setValue] = useState<Descendant[] | null>(null)
     const id = useRef(`${Date.now()}`);
     const remote = useRef(false);
+    const renderLeaf = useCallback(props => <Leaf {...props} />, [])
 
     //upon rendering the component - register a event listener
     useEffect(() => {
@@ -51,37 +59,58 @@ const SyncingEditor = ({ groupId }: PropsType) => {
         return <div>Loading...</div>;
     }
     return (
-        <Slate
-            editor={editor}
-            initialValue={value}
-            onChange={(decendants: Descendant[]) => {
-                // setValue(decendants)
-                const ops = editor.operations.filter(o => {
-                    if (o) {
-                        return (
-                            o.type !== "set_selection" &&
-                            o.type !== "merge_node" &&
-                            (!o.data || !o.data.source)
-                        );
+        <>
+            <button onMouseDown={e => {
+                e.preventDefault();
+                console.log(JSON.stringify(editor.children))
+                Editor.addMark(editor, 'bold', true)
+            }}>Bold</button>
+            <Slate
+                editor={editor}
+                initialValue={value}
+                onChange={(decendants: Descendant[]) => {
+                    console.log('change detected')
+                    // setValue(decendants)
+                    const ops = editor.operations.filter(o => {
+                        if (o) {
+                            return (
+                                o.type !== "set_selection" &&
+                                o.type !== "merge_node" &&
+                                (!o.data || !o.data.source)
+                            );
+                        }
+
+                        return false;
+                    })
+                        .map((o: any) => ({ ...o, data: { source: "one" } }));
+
+                    if (ops.length && !remote.current) {
+                        socket.emit("new-operations", {
+                            editorId: id.current,
+                            ops: JSON.stringify(ops),
+                            value: decendants,
+                            groupId: groupId
+                        });
                     }
-
-                    return false;
-                })
-                    .map((o: any) => ({ ...o, data: { source: "one" } }));
-
-                if (ops.length && !remote.current) {
-                    socket.emit("new-operations", {
-                        editorId: id.current,
-                        ops: JSON.stringify(ops),
-                        value: decendants,
-                        groupId: groupId
-                    });
-                }
-            }}
-        >
-            <Editable />
-        </Slate >
+                }}
+            >
+                <Editable
+                    renderLeaf={renderLeaf}
+                    onKeyDown={(event) => {
+                        switch (event.key) {
+                            case 'b': {
+                                event.preventDefault()
+                                console.log('b pressed')
+                                Editor.addMark(editor, 'italic', true)
+                                break
+                            }
+                        }
+                    }}
+                />
+            </Slate >
+        </>
     )
 }
 
 export default SyncingEditor;
+
